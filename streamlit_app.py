d9e0e0f6d69c7cc9812f98ce48197bf2cda29377
Ryan Liam <94153321+R-otato@users.noreply.github.com>
2023-04-30 19:36:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ def get_fruityvice_data(this_fruit_choice):
   # write your own comment - what does this do?
   return fruityvice_normalized
 
-streamlit.header("Fruityvice Fruit Advice!"
+streamlit.header("Fruityvice Fruit Advice!")
 
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
